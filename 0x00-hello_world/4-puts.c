6d01891919fa6821f15a3c5059910b40913aa929
Daniel Okosun <okosunsunogieva@gmail.com>
2022-10-13 12:30:09 -0700

    Corrected Betty warning on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 
 /**
   * main - function name
-  * Return - return (0)
+  * Return: Returns 0
   */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
