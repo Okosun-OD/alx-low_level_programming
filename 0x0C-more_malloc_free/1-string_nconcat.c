e96fcb748d1d17ca2a6b7c9022235b4b5f4d541c
Daniel Okosun <okosunsunogieva@gmail.com>
2022-11-10 13:01:17 -0800

    Made corrections

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,7 +6,7 @@
   * @s2: string 2
   * @n: unsigned int
   * Return: returned pointer shall point to a newly allocated space in memory,
-  *which contains s1, followed by the first  *n bytes of s2, and null terminated
+  *which contains s1, followed by the first  *n bytes of s2, and null
   *If the function fails, it should return NULL
   *If n is greater or equal to the length of s2 then use the entire string s2
   *if NULL is passed, treat it as an empty string
