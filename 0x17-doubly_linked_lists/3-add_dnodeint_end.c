19917d444d6d05553d4f2623567c160cb2d4d153
Daniel Okosun <okosunsunogieva@gmail.com>
2023-01-12 10:57:12 -0800

    Added Repo

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+  * add_dnodeint_end - Makes the sum of two numbers
+  * @head: First operand
+  * @n: Second operand
+  *
+  * Return: The sum of the two parameters
+  */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *ptr = *head;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (ptr == NULL)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+		new_node->next = ptr;
+		return (new_node);
+	}
+
+	while ((ptr != NULL) && (ptr->next != NULL))
+		ptr = ptr->next;
+
+	ptr->next = new_node;
+	new_node->prev = ptr;
+	new_node->next = NULL;
+
+	return (new_node);
+}
